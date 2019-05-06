d1468cd1f4343f7c10a71903107c7a4b5bf3e275
Austin Suarez <austinmsuarez@gmail.com>
2019-05-05 23:22:30 -0700

    No longer need to recreate db every run. Password for that user is changed and lives in azure encrypted.

--- junkerPoS/db_manager.py
+++ junkerPoS/db_manager.py
@@ -9,31 +9,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def refresh():
-    logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    logger = logging.getLogger(__name__)
+    pass
+    #logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    #logger = logging.getLogger(__name__)
 
-    client = document_client.DocumentClient(config_cosmos.COSMOSDB_HOST, {'masterKey': config_cosmos.COSMOSDB_KEY})
+    #client = document_client.DocumentClient(config_cosmos.COSMOSDB_HOST, {'masterKey': config_cosmos.COSMOSDB_KEY})
 
-    # Attempt to delete the database.  This allows this to be used to recreate as well as create
-    try:
-        db = next((data for data in client.ReadDatabases() if data['id'] == config_cosmos.COSMOSDB_DATABASE))
-        client.DeleteDatabase(db['_self'])
-    except:
-        pass
+    ## Attempt to delete the database.  This allows this to be used to recreate as well as create
+    #try:
+    #    db = next((data for data in client.ReadDatabases() if data['id'] == config_cosmos.COSMOSDB_DATABASE))
+    #    client.DeleteDatabase(db['_self'])
+    #except:
+    #    pass
 
-    # Create database
-    db = client.CreateDatabase({ 'id': config_cosmos.COSMOSDB_DATABASE })
+    ## Create database
+    #db = client.CreateDatabase({ 'id': config_cosmos.COSMOSDB_DATABASE })
 
-    # Create collection
-    collection = client.CreateCollection(db['_self'],{ 'id': config_cosmos.COSMOSDB_COLLECTION })
+    ## Create collection
+    #collection = client.CreateCollection(db['_self'],{ 'id': config_cosmos.COSMOSDB_COLLECTION })
 
-    # Create document
-    document = client.CreateDocument(collection['_self'],
-        {  "id": "1234",
-           "employeeName": "John Snow",
-           "username": "kingofthenorth",
-           "password": "pbkdf2:sha256:50000$MeT7QKTp$6f5827542861370090c30f40a461ed9a9ec9da6b8ec6facf205875ec003c5204"
-        })
 
 def validate_user(username, password):
     client = document_client.DocumentClient(config_cosmos.COSMOSDB_HOST, {'masterKey': config_cosmos.COSMOSDB_KEY})
